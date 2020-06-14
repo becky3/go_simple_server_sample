38c3cfbb9a0213a2d77b0fd92fa6224b6e58547e
becky <>
2020-06-14 10:40:47 +0900

    国際化ドメイン

---
+++ 03/change_ascii/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/idna"
+)
+
+func main() {
+
+	src := "日本語ドメイン.com"
+	ascii, err := idna.ToASCII(src)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s -> %s\n", src, ascii)
+
+}
